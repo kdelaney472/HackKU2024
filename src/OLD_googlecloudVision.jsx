bccf532e1302a28295bc1ea32e31f84d24ae1c9d
Kieran Delaney <kdelaney472@gmail.com>
2024-04-13 16:27:14 -0500

    switched to new image recognition model from huggingface

--- src/OLD_googlecloudVision.jsx
+++ src/OLD_googlecloudVision.jsx
@@ -1,32 +1,40 @@
 import { useState, useEffect } from "react";
 import {Image, Listbox, ListboxItem} from "@nextui-org/react";
 import axios from 'axios' //for making call to vision api
-import env from '../HuggingFace_API_KEY.json' //store your key here locally (DO NOT PUSH TO GITHUB)
+import env from '../GoogleCloud_API_KEY.json' //store your key here locally (DO NOT PUSH TO GITHUB)
 const API_KEY = env.API_KEY;
 
 function ImageSubmit() {
     const [imgFile, setFile] = useState(null);
     const [foodLabels, setLabels] = useState([]);
 
-    const aiVisionScan = async (img) => {
-        const apiURL = `https://api-inference.huggingface.co/models/Kaludi/food-category-classification-v2.0`;
+    const aiVisionScan = async (img, foods) => {
+        const apiURL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`;
         const base64Img = img.split(',')[1]; //gets the base64 content from the base64 encoded url (the first portion is the header "data:image/png;base64," and the second portion at index 1 has the data we need to send)
-        const data = {
-            image: base64Img,
+        const request = {
+            requests: [
+                {
+                    image: {
+                        content: base64Img,
+                    },
+                    features: [{type: 'LABEL_DETECTION', maxResults: 5}],
+                }
+            ]
         };
-        const param = {
-            headers: { 
-                'Authorization': `Bearer ${API_KEY}`,
-                'Content-Type': 'application/json',
-            },
-        };
-        const response = await axios.post(apiURL, data, param);
-        setLabels(response.data);
+        const response = await axios.post(apiURL, request);
+        //console.log(response.data.responses[0].labelAnnotations);
+        if(foods){
+            if(foods === response.data.responses[0].labelAnnotations)
+                return;
+        }
+        setLabels(response.data.responses[0].labelAnnotations);
     }
 
     useEffect( () => { //only runs when the image changes
         if(imgFile){
-            aiVisionScan(imgFile);
+            console.log(imgFile);
+            setLabels([]);
+            aiVisionScan(imgFile, foodLabels);
         }
     }, [imgFile])
  
@@ -53,11 +61,9 @@ function ImageSubmit() {
             {(foodLabels!=[]) && (
                 <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
                     <Listbox aria-label="theFoodLabels">
-                        {foodLabels.map((foodInfo, id) => (
-                            <ListboxItem key={id} onClick={()=>(alert(foodInfo.label))}>
-                                {id+"\t"}
-                                {foodInfo.label+"\t"}
-                                {foodInfo.score+"\t"}
+                        {foodLabels.map((label) => (
+                            <ListboxItem key={label.mid} onClick={()=>(alert(label.description))}>
+                                {label.description}
                             </ListboxItem>
                         ))}
                     </Listbox>
