11fdb2fed8dfd425dd7623e8568fe206e13042de
PaulStuever <pstuever27@gmail.com>
2024-04-13 20:37:13 -0500

    try again

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-
-
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
